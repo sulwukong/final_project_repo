1b5dc619ea0abdfd4aa10a76fa477cf5f1e1884a
Sulman Zahid <szahid1@umd.edu>
2024-05-17 22:23:25 -0400

    added to word bank

--- consolidation_final_proj.py
+++ consolidation_final_proj.py
@@ -1,11 +1,12 @@
-# pair programming practice
+# Word Guessing Game
 
 import os
 import random
 
 word_bank = ["flexible", "paralyzed", "summit", "profession", "challenge", "entertain", 
              "proclaim", "greeting", "justice", "classify", "button", "appeal", "settlement", "salad",
-             "character", "land", "total"]
+             "character", "land", "total", "plaintiff", "leadership", "guest", "obligation", "cooperative", "summary",
+             "demonstration", "graphic"]
 
 
 word = random.choice(word_bank)
@@ -15,6 +16,8 @@ guess_count = 0
 score = 0
 remaining_tries = 3
 
+print(f"The word is {wordlength} characters long.")
+
 while guess_count < 3:
 
     guess = str(input("Guess a letter or word:  \n"))
@@ -23,9 +26,9 @@ while guess_count < 3:
     guess_short = guess.strip()
 
     if length == 1:
-        count = word.count(guess)
+        count = word.count(guess_short)
         score += 1
-        print(f"The letter '{guess}' appeared {count} times in the word. Your score is {score}.")
+        print(f"The letter '{guess_short}' appeared {count} times in the word. Your score is {score}.")
 
 
     elif length > 1:
@@ -36,4 +39,6 @@ while guess_count < 3:
             guess_count += 1
             remaining_tries = 3 - guess_count
             print(f"{guess_short} is not the word. You have {remaining_tries} attempts left.")
+
+print(f"Your score was {score}")
 
